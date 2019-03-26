df0f52cb33e6936c30536b40f95848e631e205ea
active-object-6703 <nerajno@gmail.com>
2019-03-26 02:28:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,36 @@
 # Write your code here.
+#badge_maker
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+#batch_badge_creator
+def batch_badge_creator(attendees)
+  badges = []
+    for attendant in attendees
+    badges.push(badge_maker(attendant))
+  end
+  return badges
+end
+
+#assign_rooms
+def assign_rooms(attendees)
+  room_assignments = []
+  num = 1
+  for attendant in attendees
+    room_assignments.push("Hello, #{attendant}! You'll be assigned to room #{num}!")
+    num+=1
+  end
+  return room_assignments
+end
+
+#printer
+def printer(attendees)
+    batch_badge_creator(attendees).each do |badge|
+      puts badge
+    end
+
+    assign_rooms(attendees).each do |assignment|
+  puts assignment
+end
+end
